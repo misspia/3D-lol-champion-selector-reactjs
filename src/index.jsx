dae8afd3e6a8a2cf1973b6f72d85fa8448670d51
misspia <pia_leung@hotmail.com>
2016-12-01 12:56:35 -0500

    configure css

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
-import '../scss/index.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
