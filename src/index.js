07c2486f31c94fd55b47953579e54a77fcdefaa3
misspia <pia_leung@hotmail.com>
2016-12-01 13:07:48 -0500

    folder structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './App.scss';
+import App from './components/App';
+// import './App.scss';
+import './styles/main.scss';
 
 ReactDOM.render(
   <App />,
